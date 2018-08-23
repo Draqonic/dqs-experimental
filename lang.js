bcb1363e31a04fefd3ed63d7b2df2239d1380cef
Dranoq <dranoq@zoho.eu>
2018-08-23 03:31:13 +0300

    Some cleanups

--- lang.js
+++ lang.js
@@ -2,6 +2,7 @@
 // global braces count (indexOf)
 // disable names for vars: https://mathiasbynens.be/notes/reserved-keywords
 // info for component: class name, file
+// disable control for string: ' "" ` amd functions (move string and funcs as is)
 
 const log = console.log
 const warn = console.warning
@@ -44,7 +45,7 @@ class DranoqLangPrivateParser {
 	}
 
 	logError(error, strlen, full = false) {
-		let errorMessage = `Error ${error} in ${this.fileName}`
+		let errorMessage = /*`Error ${error} in */`${this.fileName}`
 		let col = this.column
 		if (strlen) col -= strlen + 1
 
@@ -57,7 +58,6 @@ class DranoqLangPrivateParser {
 		let currentStr = this.contents.split('\n')[this.line - 1].replace(/\t/g, '    ')
 		err(currentStr)
 		err(Array(col + 1).join(' ') + (full ? Array(strlen + 1).join('^') : '^'))
-		err()
 		err(errorMessage)
 		err()
 		err({error, errorText: errorText[error], file: this.fileName, line: this.line, column: col, str: currentStr.trim()})
@@ -119,7 +119,7 @@ class DranoqLangPrivateParser {
 				}
 
 				if (expProp.name) {
-					for(let j = 0; j !== lets.length; ++j) if (lets[j].name === str) { log('```````', str, '`'); return this.logError(6, str.length) }
+					for(let j = 0; j !== lets.length; ++j) if (lets[j].name === str) return this.logError(6, str.length, true)
 					if(str[0] !== str[0].toLowerCase()) return this.logError(5, str.length, true)
 					if (varDisableNames.indexOf(str) !== -1) return this.logError(7, str.length, true)
 					lets[lets.length - 1].name = str
@@ -160,94 +160,7 @@ class DranoqLangPrivateParser {
 		log(strs)
 		log(lets)
 
-		return
-		let q = strs
-
-		// check first char upper and first brace, get parent class name
-		let fisrtChar = q[0]['str'][0]
-		if (fisrtChar !== fisrtChar.toUpperCase())
-			return logError(1, fName, q[0]['line'], orig_split[q[0]['line'] - 1])
-		parentName = q[0]['str']
-		
-		// check braces
-		if (q[1].str !== '{')
-			return logError(2, fName)
-		if (q[q.length - 1].str !== '}')
-			return logError(3, fName)
-		
-		//
-		let props = []
-		let slots = []
-		let funcs = []
-		let signs = []
-		let brace = 0
-		let currentProperty = false
-		let maybeValue = false
-		let maybeOn = false
-
-		for(let i = 1; i !== q.length; ++i) {
-			console.log(q[i])
-			let str = q[i].str
-			let line = q[i].str
-
-			if (str === '{') brace++
-			if (str === '}') brace--
-
-			if (str === 'property') {
-				currentProperty = true
-				props.push({name: '', type: '', value: []})
-				continue
-			}
-
-			if (currentProperty) {
-				let prop = props[props.length - 1]
-				if (!prop.type)
-					prop.type = str
-				else {
-					if(str[str.length - 1] === ':') str = str.substring(0, str.length - 1)
-					prop.name = str
-					currentProperty = false
-					maybeValue = true
-				}
-				continue
-			}
-			
-			let ifOn = str.substring(0, 2) === 'on'
-			if (ifOn) { maybeOn = true; if(str[str.length - 1] === ':') str = str.substring(0, str.length - 1); slots.push({name: str, value: []}) }
-			if (maybeValue) {
-				//log('maybe', str, keyWords.indexOf(str) !== -1)
-				if (ifOn || keyWords.indexOf(str) !== -1) {
-					maybeValue = false
-					if (!ifOn) maybeOn = false
-					continue
-				}
-				props[props.length - 1].value.push(str)
-				continue
-			}
-
-			if (maybeOn) {
-				if (!ifOn)
-					slots[slots.length - 1].value.push(str)
-				continue
-			}
-		}
-		
-		for(const slot of slots) {
-			if (slot.value[0] === ':') slot.value.splice(0, 1);
-			if(slot.value[0] === '{' && slot.value[slot.value.length - 1] === '}') {
-				slot.value.splice(0, 1); slot.value.splice(slot.value.length - 1, 1)
-			}
-			slot.value = slot.value.join(' ')
-		}
-		
-		log('Brace =', brace)
-		if (brace)
-			logError(0, fName, 0, '')
-
-		//log('parentName', parentName)
-		log(slots)
-		log(props)
-		//log('Lines:', line)
+		return true
 	}
 }
 
