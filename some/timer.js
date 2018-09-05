4fa933a05afc5a23e08bd3054f64d1f88480d2e1
Dranoq <dranoq@zoho.eu>
2018-09-05 20:09:06 +0300

    Upgrade Timer

--- some/timer.js
+++ some/timer.js
@@ -14,38 +14,53 @@ class Timer extends DSObject {
 	}
 
 	start() {
-		if (this.triggeredOnStart)
+		if (this.runOnStart)
 			this.triggered()
-		this.running = true
+		this.active = true
 	}
 
 	stop() {
-		this.running = false
+		this.active = false
+	}
+
+	restart() {
+		this._renewTimer()
 	}
 
 	_renewTimer() {
+		// console.log('renew')
 		if (this.priv.timer)
 			clearTimeout(this.priv.timer)
-		if (!this.running) return
+		if (!this.active) return
 
 		if (this.repeat)
 			this.priv.timer = setInterval(this.triggered.bind(this), this.interval)
 		else
 			this.priv.timer = setTimeout(this.triggered.bind(this), this.interval)
+		this.startTime = new Date()
+	}
+
+	leftSeconds() {
+		if (!this.active) return 0
+		return Math.ceil((this.priv.timer._idleStart + this.priv.timer._idleTimeout - (new Date - this.startTime)) / 1000)
 	}
 }
+
 Timer.property('interval', 'int', 1000)
-Timer.property('running', 'bool')
+Timer.property('active', 'bool')
 Timer.property('repeat', 'bool')
-Timer.property('triggeredOnStart', 'bool')
+Timer.property('runOnStart', 'bool')
+
 Timer.signal('triggered')
-Timer.on('triggered', () => {
-	if (!this.repeat && (!this.triggeredOnStart || this._triggered))
-		this.running = false
+Timer.on('triggered', function() {
+	if (!this.repeat) // TODO: && (!this.runOnStart || this._triggered))
+		this.active = false
 })
 
-Timer.change(['running', 'interval', 'repeat'], function() {
+Timer.change(['active', 'interval', 'repeat'], function(value) {
 	this._renewTimer()
 })
+
+// Timer.change('active', (value) => console.log('active', value))
 
 module.exports = Timer
